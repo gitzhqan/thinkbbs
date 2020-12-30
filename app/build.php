d2576100ec88923c6432d97d36171a8a60540240
gitzhqan <ali_zhqan@aliyun.com>
2020-12-30 16:42:27 +0800

    前端布局模板

--- app/build.php
+++ app/build.php
@@ -4,5 +4,7 @@ return [
     // 需要自动创建的文件
     '__file__'   => [],
     // common 需要自动创建的目录
-    '__dir__'    => ['model', 'exception', 'validate'],
+    '__dir__'    => ['view'],
+    // 需要自动创建的控制器
+    'controller' => [],
 ];
